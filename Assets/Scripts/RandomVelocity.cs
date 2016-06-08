c158aec2d2823a6b3abe7e4b05886e04db414ba0
kdo70 <kdo70@yahoo.com>
2016-06-07 20:56:32 -0700

    Random Velocity Script Tweaks
    -When an object spawns on the left side, it flips and rotates clockwise

--- Assets/Scripts/RandomVelocity.cs
+++ Assets/Scripts/RandomVelocity.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class RandomVelocity : MonoBehaviour {
 	Rigidbody2D rb2d;
+	SpriteRenderer sr;
 	public float minSpeed;
 	public float maxSpeed;
 	public float rotateSpeed = 10f;
@@ -12,6 +13,7 @@ public class RandomVelocity : MonoBehaviour {
 	public float yOffset = 7f;
 	void Awake () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
 	}
 
 	void Start () {
@@ -20,12 +22,18 @@ public class RandomVelocity : MonoBehaviour {
 		ySpeed = Random.Range (-maxSpeed, maxSpeed);
 		transform.position = new Vector3 (xSpeed > 0 ?  -xOffset: xOffset, ySpeed > 0 ?  -yOffset: yOffset,0);
 		rb2d.AddForce (new Vector2 (xSpeed > 0 ? xSpeed + minSpeed : xSpeed - minSpeed, ySpeed > 0 ? ySpeed + minSpeed : ySpeed - minSpeed));
-
+		if(xSpeed > 0) {
+			sr.flipX = true;
+		}
 	}
 	
 
 	void FixedUpdate () {
-		rb2d.MoveRotation (rb2d.rotation + rotateSpeed * Time.fixedDeltaTime);
+		if (xSpeed > 0) {
+			rb2d.MoveRotation (rb2d.rotation - rotateSpeed * Time.fixedDeltaTime);
+		} else {
+			rb2d.MoveRotation (rb2d.rotation + rotateSpeed * Time.fixedDeltaTime);
+		}
 	}
 		
 }
